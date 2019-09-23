86ca4b3a713d35efce8617b70c8e1d4f47e1dac6
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-23 15:23:43 -0700

    Two sums attempt

--- exercises/LC1_TwoSum/index.js
+++ exercises/LC1_TwoSum/index.js
@@ -6,6 +6,25 @@
 // twoSum([2,7,11,15], 9) --> [0,1]
 // twoSum([3,2,4], 9) --> [1,2]
 
-function twoSum(arr, target) {}
+function twoSum(arr, target) {
+    /*
+    Input: Array of numbers and target
+    Output: Indices of two numbers
+
+    Steps: Time: O(N*(N-1)) = O(N^2), Space: O(1)
+      - Go through all possible pairs 
+          - If found a pair then return
+
+    Assumptions: There will always be a pair
+
+    */
+   for (let i = 0; i < arr.length; i++) {
+       for (let x = i+1; x < arr.length; x++) {
+           if (arr[i]+arr[x] === target) return [i,x];
+       }
+   }
+   return -1;
+
+}
 
 module.exports = twoSum;
