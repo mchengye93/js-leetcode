9ec8299575a8e961e6db4f4bc63ab3d197206d8b
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-23 15:57:26 -0700

    Attempt maxArea problem

--- exercises/LC11_maxArea/index.js
+++ exercises/LC11_maxArea/index.js
@@ -11,6 +11,38 @@ Output: 49
 */
 
 
-function maxArea(height) {}
+function maxArea(height) {
+    /*
+    Input: arr of heights
+    Output: int , max area
+
+    Steps:
+    -Go through array and record left-bound highest height
+    -Go through and record right bound highest height
+    - Keep track of n distance between left and right bound
+    */
+
+    let left = height[0];
+    let leftIndex = 0;
+    let right = height[1];
+    let rightIndex = 1;
+    let n = rightIndex-leftIndex;
+    let maxArea = 0;
+
+    for (let i = 2; i < height.length; i++) {
+        n = rightIndex-leftIndex;
+        let area = Math.min(left,right) * n;
+
+        if (area > maxArea) {
+            maxArea = area;
+        }
+        if (height[i] <= left) {
+            right = height[i];
+            rightIndex = i;
+        } 
+        
+       
+    }
+}
 
 module.exports = maxArea;
