1c008eaed189a38e5c2a64f0ff7dfbeecae74b8c
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-24 22:23:25 -0700

    MVP 3sums problem optimal solution

--- exercises/LC15_threeSum/index.js
+++ exercises/LC15_threeSum/index.js
@@ -34,10 +34,17 @@ const threeSum = nums => {
         let left = i + 1;
         let right = nums.length-1;
 
+        if (i > 0 && nums[i] === nums[i-1]) {
+            continue;
+        }
+
         while (left < right ) {
             if (nums[left] + nums[right] === target) {
-                result.push(nums[i], nums[left], nums[right]);
+                result.push([nums[i], nums[left], nums[right]]);
                 left++;
+                while(nums[left] === nums[left-1]) {
+                    left++;
+                }
 
             } else if ( nums[left] + nums[right] < target) {
                 left++;
@@ -47,6 +54,7 @@ const threeSum = nums => {
         }
 
     }
+    return result;
 };
 
 module.exports = threeSum;
