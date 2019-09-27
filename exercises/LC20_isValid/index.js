98616c2a18a181662b5e599955c0bd2bd44c53fc
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-26 21:52:35 -0700

    MVP isValid problem

--- exercises/LC20_isValid/index.js
+++ exercises/LC20_isValid/index.js
@@ -1,3 +1,27 @@
-const isValid = s => {};
+const isValid = s => {
+    let stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        let str = s[i];
+        if(str === '{' || str === '(' || str === '[') {
+            stack.push(str);
+        } else {
+            let pop = stack.pop();
+            if (str === '}' && pop !== '{') {
+                return false;
+            } 
+
+            if (str === ')' && pop !== '(') {
+                return false
+            }
+
+            if (str === ']' && pop !== '[') {
+                return false;
+            }
+            
+        }
+    }
+    return stack.length === 0;
+};
 
 module.exports = isValid;
