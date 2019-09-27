2eeb56165ad2e5a07ffe54534fe8e67486825db3
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-26 21:12:09 -0700

    Attempt longestSubstring

--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,29 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
+function lengthOfLongestSubstring(s) {
+ let letters= {};
+
+
+ let longestSubstring = 1;
+ let count = 0;
+ for (let i = 0; i < s.length;i++) {
+     let letter = s[i];
+    if (letters[letter] === undefined) {
+        letters[letter] = i;
+        count++;
+
+        if (count > longestSubstring) {
+            longestSubstring = count;
+        }
+        
+    } else {
+        count = 1;
+        letters = {};
+        letters[letter] = i;
+    }
+ }
+ return longestSubstring;
+}
 
 module.exports = lengthOfLongestSubstring;
