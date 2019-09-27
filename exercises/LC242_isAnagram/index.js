ff49844f74bf8d71bb6fae8aece5676ce3f9648e
Marbo Cheng Ye <marbochengye@gmail.com>
2019-09-26 22:31:20 -0700

    Optimal isAnagram solution

--- exercises/LC242_isAnagram/index.js
+++ exercises/LC242_isAnagram/index.js
@@ -20,7 +20,30 @@ function isAnagram(s, t) {
     if (s.length !== t.length) {
         return false;
     }
-    return s.split('').sort().join('') === t.split('').sort().join('');
+    
+    const sCharCount = {};
+
+    for (let i = 0; i < s.length; i++) {
+        if (!sCharCount[s[i]]) {
+            sCharCount[s[i]] = 1;
+        } else {
+            sCharCount[s[i]]++;
+        }
+         
+    }
+
+    for (let i = 0 ; i < t.length; i++) {
+        if(!sCharCount[t[i]]) {
+            return false;
+        } else {
+            sCharCount[t[i]]--;
+
+            if (sCharCount[t[i]] < 0) {
+                return false;
+            }
+        }
+    }
+    return true;
 
 }
 
