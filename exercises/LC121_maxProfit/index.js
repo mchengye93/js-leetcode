b8affb6320735262df96ff91c879580512d2ebd3
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-03 21:37:21 -0700

    MVP maxProfit problem

--- exercises/LC121_maxProfit/index.js
+++ exercises/LC121_maxProfit/index.js
@@ -1,3 +1,19 @@
-var maxProfit = function(prices) {};
+var maxProfit = function(prices) {
+    let maxProfit = 0;
+    let minPrice = prices[0];
+
+    for (let i = 0; i < prices.length; i++) {
+        let price = prices[i];
+        if (price < minPrice) {
+            minPrice = price;
+        } else {
+            let profit = price - minPrice;
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+    }
+    return maxProfit;
+};
 
 module.exports = maxProfit;
