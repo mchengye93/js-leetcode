a722be70a61bdc23263cd33b10ffc41f3792ee3a
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-19 20:47:45 -0700

    find min in rotate array MVP

--- exercises/LC153_findMin/index.js
+++ exercises/LC153_findMin/index.js
@@ -16,7 +16,44 @@ Example 2:
 Input: [4,5,6,7,0,1,2]
 Output: 0
 */
+
+
 function findMin(nums) {
+    let left = 0;
+    let right = nums.length-1;
+
+    if (nums.length ===1) {
+        return nums[0];
+    }
+   
+    if (nums[left] < nums[right]) {
+        return nums[left];
+    }
+
+    while(left <= right) {
+        const mid = Math.floor((left+right)/2);
+        const leftVal = nums[left];
+        const midVal = nums[mid];
+
+        const leftOfMid = nums[mid-1];
+        const rightOfMid = nums[mid+1];
+
+        //we are at inflection point
+        if(midVal > rightOfMid) {
+            return rightOfMid;
+        } else if (leftOfMid > midVal) {
+            //we are at lowest value since left is greater
+            return midVal;
+        }
+
+        if(midVal > leftVal) {
+            left = mid +1;
+        } else {
+            right = mid -1;
+        }
+
+
+    }
 
 }
 
