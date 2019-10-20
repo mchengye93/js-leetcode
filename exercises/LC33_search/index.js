ab3cd0209d763cc5a4ea7a91393c46310a238671
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-19 21:01:21 -0700

    binarySearch function

--- exercises/LC33_search/index.js
+++ exercises/LC33_search/index.js
@@ -21,6 +21,38 @@ Input: nums = [4,5,6,7,0,1,2], target = 3
 Output: -1
 */
 
-function search(nums, target) {}
+function search(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left+right)/2);
+        let midVal = nums[mid];
+        if (midVal === target) return mid;
+
+        let leftOfMid = nums[mid-1];
+        let rightOfMid = nums[mid+1];
+
+        if (leftOfMid < midVal &&  target < midVal) {
+            left = mid - 1;
+        } else {
+            
+        }
+
+    }
+}
+
+function binarySearch(arr,target,left,right) {
+    while (left <=right) {
+        const mid = Math.floor((left+right)/2);
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid+ 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+}
 
 module.exports = search;
