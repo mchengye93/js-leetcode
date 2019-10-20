ef9c298e465cfdb14d235a9f290680ecccb306b4
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-19 20:03:29 -0700

    MVP sameTree problem

--- exercises/LC100_isSameTree/index.js
+++ exercises/LC100_isSameTree/index.js
@@ -34,6 +34,24 @@ Output: false
 */
 
 function isSameTree(p, q) {
+    let sameTree = true;
+
+    function checkSameNode(p,q) {
+        if(!p && !q) {
+            return;
+        } else if (!p || !q) {
+            sameTree = false;
+            return;
+        } else if (p.val !== q.val) {
+            sameTree = false;
+            return;
+        }
+        checkSameNode(p.left,q.left);
+        checkSameNode(p.right,q.right);
+    }
+    checkSameNode(p,q);
+    
+    return sameTree;
 
 }
 
