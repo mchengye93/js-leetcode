ca9cb6f27f8db4294afb66f83ad1630c5005f453
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-19 20:17:19 -0700

    levelOrderMVP

--- exercises/LC102_levelOrder/index.js
+++ exercises/LC102_levelOrder/index.js
@@ -1,3 +1,25 @@
-function levelOrder(root) {}
+function levelOrder(root) {
+    if(root === null) return [];
+  let stack = [];
+  let result = [];
+  stack.push(root);
+
+  while(stack.length > 0) {
+
+    let temp = [];
+    let size = stack.length;
+
+    for (let i = 0 ; i < size; i++) {
+      let node = stack.shift();
+      temp.push(node.val);
+
+      if(node.left) stack.push(node.left);
+      if(node.right)stack.push(node.right);
+    }
+    result.push(temp);
+
+  }
+  return result;
+}
 
 module.exports = levelOrder;
