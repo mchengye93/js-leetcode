8036c00178e5bb986dd4b668c15b14c864624f54
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-19 20:08:03 -0700

    time complexity maxDepth

--- exercises/LC104_maxDepth/index.js
+++ exercises/LC104_maxDepth/index.js
@@ -1,5 +1,6 @@
+
+//Runtime: O(N) go through all nodes and O(n) if recursive call count as space;
 function maxDepth(root) {
-    
     if (root === null) return 0;
     return Math.max(maxDepth(root.left)+1, maxDepth(root.right)+1);
 }
