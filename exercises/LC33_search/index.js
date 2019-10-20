623850d17777d614263ee3b10745d8724e501a3c
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-19 21:21:36 -0700

    rotateSearch MVP

--- exercises/LC33_search/index.js
+++ exercises/LC33_search/index.js
@@ -21,38 +21,65 @@ Input: nums = [4,5,6,7,0,1,2], target = 3
 Output: -1
 */
 
-function search(nums, target) {
+
+
+function findInflectionIdx(rotatedSortedArr) {
     let left = 0;
-    let right = nums.length - 1;
+    let right = rotatedSortedArr.length-1;
 
-    while (left <= right) {
-        let mid = Math.floor((left+right)/2);
-        let midVal = nums[mid];
-        if (midVal === target) return mid;
+    if (rotatedSortedArr.length === 1) {
+        return 0;
+    }
 
-        let leftOfMid = nums[mid-1];
-        let rightOfMid = nums[mid+1];
+    //if first is less than last it means it is sorted arr and not rotated
+    if(rotatedSortedArr[left] < rotatedSortedArr[right]) {
+        return 0;
+    }
 
-        if (leftOfMid < midVal &&  target < midVal) {
-            left = mid - 1;
-        } else {
-            
+    while( left <= right ) {
+        const mid = Math.floor((left+right)/2);
+
+        //we have a inflection point where left is less than mid
+        if (rotatedSortedArr[mid] < rotatedSortedArr[mid-1]) {
+            return mid;
         }
 
+        //if current mid is greater than next value then inflection point is next val
+        if (rotatedSortedArr[mid] > rotatedSortedArr[mid+1]){
+            return mid + 1; 
+        }
+
+        if(rotatedSortedArr[mid] < rotatedSortedArr[left]) {
+            right = mid-1;
+        } else {
+            left = mid + 1;
+        }
     }
 }
 
 function binarySearch(arr,target,left,right) {
-    while (left <=right) {
+    while (left <= right) {
         const mid = Math.floor((left+right)/2);
         if (arr[mid] === target) {
             return mid;
         } else if (arr[mid] < target) {
-            left = mid+ 1;
+            left = mid + 1;
         } else {
             right = mid - 1;
         }
     }
+    return - 1;
+}
+
+
+function search(nums, target) {
+    const minIdx = findInflectionIdx(nums);
+
+    const left = binarySearch(nums,target,0, minIdx- 1);
+    const right = binarySearch(nums,target,minIdx,nums.length-1);
+
+    return Math.max(left,right);
+
 }
 
 module.exports = search;
