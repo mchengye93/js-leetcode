0c177615c15a245edfd05d7f9a50e64e35fb433f
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-19 20:06:07 -0700

    maxDepth MVP

--- exercises/LC104_maxDepth/index.js
+++ exercises/LC104_maxDepth/index.js
@@ -1,3 +1,7 @@
-function maxDepth(root) {}
+function maxDepth(root) {
+    
+    if (root === null) return 0;
+    return Math.max(maxDepth(root.left)+1, maxDepth(root.right)+1);
+}
 
 module.exports = maxDepth;
