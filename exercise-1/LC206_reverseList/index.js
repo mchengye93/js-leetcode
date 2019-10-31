b2f057ef17d51c8a80a2bff3110aedd7691939e2
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-31 14:02:40 -0700

    reverseLinkedList MVP

--- exercise-1/LC206_reverseList/index.js
+++ exercise-1/LC206_reverseList/index.js
@@ -1,3 +1,20 @@
-const reverseList = head => {};
+const reverseList = head => {
+
+    let prev = null;
+    let current = head;
+
+    let next = current.next;
+
+    while (current) {
+        let temp = current.next;
+
+        current.next = prev;
+        prev = current;
+        current = temp;
+    }
+
+    return prev;
+    
+};
 
 module.exports = reverseList;
