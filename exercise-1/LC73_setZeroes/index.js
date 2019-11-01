d3afcc7cc606ee9452ad59ce8ba287e4bf35e3db
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-31 18:24:13 -0700

    zero matrix problem

--- exercise-1/LC73_setZeroes/index.js
+++ exercise-1/LC73_setZeroes/index.js
@@ -33,6 +33,55 @@ Output:
 */
 
 function setZeroes(matrix) {
+    let firstColHasZero = false;
+    let firstRowHasZero = false;
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (matrix[i][0] === 0) {
+            firstColHasZero = true;
+            break;
+        }
+    }
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (matrix[0][i] === 0) {
+            firstRowHasZero = true;
+            break;
+        }
+    }
+
+    //use first row and column s flags, if rest of cell have zeroes
+    for (let row = 1; row < matrix.length; row++) {
+        for (let col = 1; col < matrix[0].length; col++) {
+            if (matrix[row][col] === 0) {
+                matrix[0][col] = 0;
+                matrix[row][0] = 0;
+            }
+        }
+    }
+    //Zero out cells based on flags in first row and column
+    for (let row = 1; row < matrix.length; row++) {
+        for (let col = 1; col < matrix[0].length; col++) {
+            if (matrix[row][0] === 0 || matrix[0][col] === 0) {
+                matrix[row][col] = 0;
+            }
+        }
+    }
+
+    //zero out first colum
+    if (firstColHasZero) {
+        for (let i = 0; i < matrix.length; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+
+    //zero first row
+    if (firstRowHasZero) {
+        for (let i = 0; i < matrix[0].length; i++) {
+            matrix[0][i] = 0;
+        }
+    }
+
 
 }
 
