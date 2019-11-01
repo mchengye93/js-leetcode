6fdddc4bbf6dcce781d69097cde48dbe1ab788df
Marbo Cheng Ye <marbochengye@gmail.com>
2019-10-31 17:42:26 -0700

    mergeTwoList linkedlist

--- exercise-1/LC21_mergeTwoLists/index.js
+++ exercise-1/LC21_mergeTwoLists/index.js
@@ -7,8 +7,29 @@ Example:
 Input: 1->2->4, 1->3->4
 Output: 1->1->2->3->4->4
 */
+/* Time: O(N) Space = O(1)
+*/
 function mergeTwoLists(l1, l2) {
 
+    let dummyHead = {next: null};
+    let tail = dummyHead;
+
+    while(l1 !== null && l2 !== null) {
+        if (l1.val < l2.val) {
+            tail.next = l1;
+            tail = tail.next;
+            l1 = l1.next;
+        } else {
+            tail.next = l2;
+            tail = tail.next;
+            l2 = l2.next;
+        }
+    }
+
+    tail.next = l1 || l2;
+
+    return dummyHead.next;
+    
 }
 
 
